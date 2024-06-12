78afef4a3e5f352b9d0a46fd4a6281d903fbbce5
Victor <luanvitorlima2017@gmail.com>
2024-06-12 16:58:21 -0300

    projeto finalizado

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+const API_KEY = 'aqui fica a sua chave API'; //consulte https://openweathermap.org/api para adiquirir
+
+function WeatherApp() {
+  const [city, setCity] = useState('São Paulo');
+  const [weatherData, setWeatherData] = useState(null);
+
+  const handleSearch = async () => {
+    if (!city) {
+      console.warn('Please enter a city name to search for weather.');
+      return; 
+    }
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&lang=pt_br&units=metric`);
+    const data = await response.json();
+    setWeatherData(data);
+  };
+
+  useEffect(() => {
+    handleSearch();
+  }, []); 
+
+  if (!weatherData) return <p>Carregando...</p>;
+
+  return (
+    <div className="bigger-box">
+      <input
+        className="input-city"
+        placeholder="Pesquisar Cidade"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+      <button className="button-search" onClick={handleSearch}>
+        <img
+          className="img-search"
+          alt="busca"
+          src="https://cdn-icons-png.flaticon.com/512/2866/2866321.png"
+        />
+      </button>
+      {weatherData && (
+        <div className="media-box">
+          <h2 className="city">Tempo em {weatherData.name}</h2>
+          <p className="temp">{Math.floor(weatherData.main.temp)}°C</p>
+          <div className="minor-box">
+            <img
+              className="icon-prevision"
+              alt="icone-info-tempo"
+              src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
+            />
+            <p className="text-prevision">{weatherData.weather[0].description}</p>
+          </div>
+          <p className="moisture">Umidade: {weatherData.main.humidity}%</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default WeatherApp;
